606ec9eb016fb17b78e804b0efcbcb916b562192
James Crasta <wtforms@jamescrasta.com>
2009-01-20 21:03:57 -0500

    Tests: added some regression unittests for Form to make sure behaviour is consistent

--- tests/form.py
+++ tests/form.py
@@ -32,6 +32,19 @@ class FormMetaTest(TestCase):
         F._m = TextField()
         self.assertEqual(F._unbound_fields, [(F.b, 'b')])
 
+    def test_subclassing(self):
+        class A(Form):
+            a = TextField()
+            c = TextField()
+        class B(A):
+            b = TextField()
+            c = TextField()
+        A(); B()
+        self.assert_(A.a is B.a)
+        self.assert_(A.c is not B.c)
+        self.assertEqual(A._unbound_fields, [(A.a, 'a'), (A.c, 'c')])
+        self.assertEqual(B._unbound_fields, [(B.a, 'a'), (B.b, 'b'), (B.c, 'c')])
+
 class FormTest(TestCase):
     class F(Form):
         test = TextField()
@@ -59,6 +72,23 @@ class FormTest(TestCase):
         form.validate()
         self.assertEqual(form.errors, {'test': ['error']})
 
+    def test_contains(self):
+        form = self.F()
+        self.assert_('test' in form)
+        self.assert_('abcd' not in form)
+
+    def test_field_removal(self):
+        form = self.F()
+        del form.test
+        self.assert_('test' not in form)
+        self.assertEqual(form.test, None)
+
+    def test_auto_populate(self):
+        m = type('Model', (object, ), {})
+        self.F(test='foobar').auto_populate(m)
+        self.assertEqual(m.test, 'foobar')
+        self.assertEqual([k for k in dir(m) if not k.startswith('_')], ['test'])
+
     def test_ordered_fields(self):
         class MyForm(Form):
             strawberry = TextField()
@@ -72,6 +102,11 @@ class FormTest(TestCase):
         self.assertEqual([x.name for x in MyForm()], ['strawberry', 'kiwi', 'apple'])
         MyForm.strawberry = TextField()
         self.assertEqual([x.name for x in MyForm()], ['kiwi', 'apple', 'strawberry'])
+
+    def test_prefixes(self):
+        self.assertEqual(self.F(prefix='foo').test.name, 'foo_test')
+        self.assertEqual(self.F(prefix='foo').test.id, 'foo_test')
+        self.assertEqual(self.F(idprefix='id_', prefix='foo').test.id, 'id_foo_test')
 
 if __name__ == '__main__':
     from unittest import main
