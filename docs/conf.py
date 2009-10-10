39f91d70e1c7a273ff095c3cc6bb1a0256655695
Thomas Johansson <prencher@prencher.dk>
2009-10-10 03:03:33 +0200

    Ready 0.4 release

--- docs/conf.py
+++ docs/conf.py
@@ -46,7 +46,7 @@ copyright = '2009 by Thomas Johansson, James Crasta'
 # The short X.Y version.
 version = '0.4'
 # The full version, including alpha/beta/rc tags.
-release = '0.4dev'
+release = '0.4'
 
 
 # There are two options for replacing |today|: either, you set today to some
