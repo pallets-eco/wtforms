29dfb7116741e2e887c27ab81830b0acccd8f5f1
Thomas Johansson <prencher@prencher.dk>
2009-10-10 03:06:43 +0200

    tip is 0.5dev

--- docs/conf.py
+++ docs/conf.py
@@ -44,9 +44,9 @@ copyright = '2009 by Thomas Johansson, James Crasta'
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '0.4'
+version = '0.5'
 # The full version, including alpha/beta/rc tags.
-release = '0.4'
+release = '0.5dev'
 
 
 # There are two options for replacing |today|: either, you set today to some
