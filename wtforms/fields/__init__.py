5f6e8ce88fda3114e91b32d31cacf7548265612e
James Crasta <wtforms@jamescrasta.com>
2011-04-27 14:26:44 -0600

    Make Flags importable too.

--- wtforms/fields/__init__.py
+++ wtforms/fields/__init__.py
@@ -1,4 +1,4 @@
 from wtforms.fields.core import *
 
 # Compatibility imports
-from wtforms.fields.core import Label, Field, _unset_value, SelectFieldBase
+from wtforms.fields.core import Label, Field, _unset_value, SelectFieldBase, Flags
