4c27552c87a24b15aa291131938a58e172c94d94
James Crasta <wtforms@jamescrasta.com>
2011-12-04 19:32:48 -0700

    setup.py overhaul:  - Support setuptools/distribute, but still support distutils.  - Support python 3.x using use_2to3  - Allow setup.py test to work  - Make unit tests into a package. Possible reorg to come.

--- tests/runtests.py
+++ tests/runtests.py
@@ -3,14 +3,30 @@ import os
 import sys
 from unittest import defaultTestLoader, TextTestRunner, TestSuite
 
-TESTS = ['form', 'fields', 'validators', 'widgets', 'webob_wrapper', 'translations']
-TESTS.extend([x for x in sys.argv[1:] if '-' not in x])
+TESTS = ('form', 'fields', 'validators', 'widgets', 'webob_wrapper', 'translations', 'ext_csrf')
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+def make_suite(prefix='', extra=()):
+    tests = TESTS + extra
+    test_names = list(prefix + x for x in tests)
+    suite = TestSuite()
+    suite.addTest(defaultTestLoader.loadTestsFromNames(test_names))
+    return suite
 
-suite = TestSuite()
-suite.addTest(defaultTestLoader.loadTestsFromNames(TESTS))
+def additional_tests():
+    """
+    This is called automatically by setup.py test
+    """
+    return make_suite('tests.')
 
-runner = TextTestRunner(verbosity=(sys.argv.count('-v') - sys.argv.count('-q') + 1))
-result = runner.run(suite)
-sys.exit(not result.wasSuccessful())
+def main():
+    extra_tests = tuple(x for x in sys.argv[1:] if '-' not in x)
+    suite = make_suite('', )
+
+    sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+    runner = TextTestRunner(verbosity=(sys.argv.count('-v') - sys.argv.count('-q') + 1))
+    result = runner.run(suite)
+    sys.exit(not result.wasSuccessful())
+
+if __name__ == '__main__':
+    main()
