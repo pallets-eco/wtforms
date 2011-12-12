fd0f773d72b2853e3cf074379b8512b71860deef
James Crasta <wtforms@jamescrasta.com>
2011-12-12 16:05:57 -0700

    Make option widget (for case of iterating a select field) take customization through kwargs

--- wtforms/widgets/core.py
+++ wtforms/widgets/core.py
@@ -229,7 +229,7 @@ class Select(object):
     rendering to make the field useful.
 
     The field must provide an `iter_choices()` method which the widget will
-    call on rendering; this method must yield tuples of 
+    call on rendering; this method must yield tuples of
     `(value, label, selected)`.
     """
     def __init__(self, multiple=False):
@@ -238,7 +238,7 @@ class Select(object):
     def __call__(self, field, **kwargs):
         kwargs.setdefault('id', field.id)
         if self.multiple:
-            kwargs['multiple'] = True 
+            kwargs['multiple'] = True
         html = [u'<select %s>' % html_params(name=field.name, **kwargs)]
         for val, label, selected in field.iter_choices():
             html.append(self.render_option(val, label, selected))
@@ -246,20 +246,20 @@ class Select(object):
         return HTMLString(u''.join(html))
 
     @classmethod
-    def render_option(cls, value, label, selected):
-        options = {'value': value}
+    def render_option(cls, value, label, selected, **kwargs):
+        options = dict(kwargs, value=value)
         if selected:
-            options['selected'] = True 
+            options['selected'] = True
         return HTMLString(u'<option %s>%s</option>' % (html_params(**options), escape(unicode(label))))
 
 
 class Option(object):
     """
-    Renders the individual option from a select field. 
-    
+    Renders the individual option from a select field.
+
     This is just a convenience for various custom rendering situations, and an
     option by itself does not constitute an entire field.
     """
     def __call__(self, field, **kwargs):
-        return Select.render_option(field._value(), field.label.text, field.checked)
+        return Select.render_option(field._value(), field.label.text, field.checked, **kwargs)
 
