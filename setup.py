0e90735a41bebec313dea37fd2bf6fa90446eaf3
James Crasta <wtforms@jamescrasta.com>
2012-02-28 11:55:27 -0700

    Tip is now 1.0.1dev

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ if sys.version_info >= (3, ):
 
 setup(
     name='WTForms',
-    version='1.0',
+    version='1.0.1',
     url='http://wtforms.simplecodes.com/',
     license='BSD',
     author='Thomas Johansson, James Crasta',
