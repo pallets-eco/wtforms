c5ddbe00f89a7a317cae6c2ae3f6262beb67c8b8
James Crasta <wtforms@jamescrasta.com>
2012-02-29 10:47:05 -0700

    Ready 1.0.1 release.

--- docs/conf.py
+++ docs/conf.py
@@ -46,7 +46,7 @@ copyright = '2010 by Thomas Johansson, James Crasta'
 # The short X.Y version.
 version = '1.0.1'
 # The full version, including alpha/beta/rc tags.
-release = '1.0.1dev'
+release = '1.0.1'
 
 
 # There are two options for replacing |today|: either, you set today to some
