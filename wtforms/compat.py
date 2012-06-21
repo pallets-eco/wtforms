88a6a0e69be57b409d2c3996adb233063def36b6
Vinay Sajip <vinay_sajip@yahoo.co.uk>
2012-06-21 22:26:39 +0100

    Further updates in response to pull request comments.

--- wtforms/compat.py
+++ wtforms/compat.py
@@ -2,24 +2,17 @@ import sys
 
 if sys.version_info[0] >= 3:
     text_type = str
-    string_types = str
+    string_types = str,
     iteritems = lambda o: o.items()
     izip = zip
     exec_ = eval("exec")
 
-    def with_metaclass(meta, base=object):
-        ns = dict(base=base, meta=meta)
-        exec_("""class NewBase(base, metaclass=meta):
-    pass""", ns)
-        return ns["NewBase"]
 else:
     text_type = unicode
-    string_types = basestring
+    string_types = basestring,
     iteritems = lambda o: o.iteritems()
     from itertools import izip
 
-    def with_metaclass(meta, base=object):
-        class NewBase(base):
-            __metaclass__ = meta
-        return NewBase
+def with_metaclass(meta, base=object):
+    return meta("NewBase", (base,), {})
 
