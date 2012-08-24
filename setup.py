203e6c586508b357e43149774cb2613c790e6713
James Crasta <wtforms@jamescrasta.com>
2012-08-24 13:24:59 -0600

    Release version 1.0.2.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ if sys.version_info >= (3, ):
 
 setup(
     name='WTForms',
-    version='1.0.2',
+    version='1.0.3',
     url='http://wtforms.simplecodes.com/',
     license='BSD',
     author='Thomas Johansson, James Crasta',
