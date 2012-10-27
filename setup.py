2b3f251dbd53ce823654068aa4d6ad8e5a2935f1
Andrei Antoukh <niwi@niwi.be>
2012-10-27 14:49:00 +0200

    Add missing classifier.

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
