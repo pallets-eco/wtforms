114067ab338017515da0c9737ea39a85b688342e
James Crasta <wtforms@jamescrasta.com>
2013-09-10 20:44:42 -0600

    Update head to 1.0.6dev

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ if sys.version_info >= (3, ):
 
 setup(
     name='WTForms',
-    version='1.0.5',
+    version='1.0.6dev',
     url='http://wtforms.simplecodes.com/',
     license='BSD',
     author='Thomas Johansson, James Crasta',
