75029d3e1985d69c42b069e08daca291652421af
Jacob Schatz <playpianolikewoah@gmail.com>
2014-05-06 08:38:14 -0400

    adding whitespace

--- wtforms/widgets/core.py
+++ wtforms/widgets/core.py
@@ -280,7 +280,7 @@ class Select(object):
             options = {}
         html = ['<select %s>' % html_params(name=field.name, **kwargs)]
         for val, label, selected in field.iter_choices():
-            html.append(self.render_option(val, label, selected,**options))
+            html.append(self.render_option(val, label, selected, **options))
         html.append('</select>')
         return HTMLString(''.join(html))
 
