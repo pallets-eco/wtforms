301306ac87b2a2da1877e413855c0079406824c7
Ronnie Beltran <rbbeltran.09@gmail.com>
2015-02-04 13:49:36 +0800

    Make populate_obj() ignore some fields

--- wtforms/form.py
+++ wtforms/form.py
@@ -84,15 +84,21 @@ class BaseForm(object):
         """
         return self.meta.get_translations(self)
 
-    def populate_obj(self, obj):
+    def populate_obj(self, obj, ignore_fields=None):
         """
         Populates the attributes of the passed `obj` with data from the form's
         fields.
 
         :note: This is a destructive operation; Any attribute with the same name
                as a field will be overridden. Use with caution.
+        :param ignore_fields:
+            Fields that should not be populated.
         """
+        if ignore_fields is None:
+            ignore_fields = []
         for name, field in iteritems(self._fields):
+            if name in ignore:
+                continue
             field.populate_obj(obj, name)
 
     def process(self, formdata=None, obj=None, data=None, **kwargs):
