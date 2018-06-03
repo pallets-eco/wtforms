15c642279793b7bde3eb8dd207e8a639a50cdbc9
David Lord <davidism@gmail.com>
2018-06-03 16:57:46 -0700

    compile MO files during develop and build

--- setup.py
+++ setup.py
@@ -2,13 +2,60 @@ from collections import OrderedDict
 import io
 import re
 
-from setuptools import setup, find_packages
+from setuptools import Command, find_packages, setup
+from setuptools.command.develop import develop as BaseDevelop
+from setuptools.command.sdist import sdist as BaseSDist
+
+try:
+    from wheel.bdist_wheel import bdist_wheel as BaseBDistWheel
+except ImportError:
+    BaseBDistWheel = None
 
 with io.open("README.rst", "rt", encoding="utf8") as f:
     readme = f.read()
 
 with io.open("wtforms/__init__.py", "rt", encoding="utf8") as f:
     version = re.search(r"__version__ = \'(.*?)\'", f.read()).group(1)
+
+
+class CompileCatalogMixin(object):
+    """Compile MO files with Babel's ``compile_catalog`` command. This
+    happens after installing in development mode, or before building
+    sdist and wheel.
+    """
+
+    def __init__(self, dist, **kw):
+        # underlying commands are old-style classes on Python 2 :-(
+        Command.__init__(self, dist, **kw)
+
+    def run(self):
+        is_develop = isinstance(self, Develop)
+
+        if not is_develop:
+            self.run_command("compile_catalog")
+
+        super(CompileCatalogMixin, self).run()
+
+        if is_develop and not self.uninstall:
+            self.run_command("compile_catalog")
+
+
+class Develop(CompileCatalogMixin, BaseDevelop):
+    pass
+
+
+class SDist(CompileCatalogMixin, BaseSDist):
+    pass
+
+
+command_classes = {"develop": Develop, "sdist": SDist}
+
+if BaseBDistWheel:
+    class BDistWheel(CompileCatalogMixin, BaseBDistWheel):
+        pass
+
+    command_classes["bdist_wheel"] = BDistWheel
+
 
 setup(
     name="WTForms",
@@ -45,8 +92,10 @@ setup(
         "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
-    packages=find_packages(),
+    packages=find_packages(exclude=("tests",)),
     include_package_data=True,
     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
+    setup_requires=["Babel"],
     extras_require={"locale": ["Babel"]},
+    cmdclass=command_classes,
 )
