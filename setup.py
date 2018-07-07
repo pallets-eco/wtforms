50a0c3c96a88f3e35abf8c6146731c5b675c596a
Alan Hamlett <alan.hamlett@gmail.com>
2018-07-07 14:37:44 -0700

    only install ipaddress for Python less than or equal 2.7

--- setup.py
+++ setup.py
@@ -99,6 +99,9 @@ setup(
     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
     setup_requires=["Babel>=2.6.0"],
     install_requires=["MarkupSafe"],
-    extras_require={"ipaddress": ["ipaddress"], "email": ["email_validator"]},
+    extras_require={
+        "ipaddress": ['ipaddress;python_version<="2.7"'],
+        "email": ["email_validator"],
+    },
     cmdclass=command_classes,
 )
