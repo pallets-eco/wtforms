149c9474c79d91aca6cb4d86c2482c4991324161
David Lord <davidism@gmail.com>
2020-04-21 10:42:59 -0700

    cleanup docs config

--- docs/conf.py
+++ docs/conf.py
@@ -1,42 +1,24 @@
-import os
-import sys
 from pallets_sphinx_themes import get_version
 from pallets_sphinx_themes import ProjectLink
-
-def _fix_import_path():
-    """
-    Don't want to pollute the config globals, so do path munging
-    here in this function
-    """
-
-    try:
-        import wtforms
-    except ImportError:
-        parent_dir = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
-        build_lib = os.path.join(parent_dir, 'build', 'lib')
-        if os.path.isdir(build_lib):
-            sys.path.insert(0, build_lib)
-        else:
-            sys.path.insert(0, parent_dir)
-
-_fix_import_path()
 
 # Project --------------------------------------------------------------
 
 project = "WTForms"
-copyright = "2008-2020 by the WTForms team"
+copyright = "2008 WTForms"
+author = "WTForms"
 release, version = get_version("WTForms")
 
 # General --------------------------------------------------------------
 
 master_doc = "index"
 extensions = [
-    "pallets_sphinx_themes",
     "sphinx.ext.autodoc",
     "sphinx.ext.doctest",
     "sphinx.ext.intersphinx",
     "sphinx.ext.viewcode",
+    "pallets_sphinx_themes",
     "sphinx_issues",
+    "sphinxcontrib.log_cabinet",
 ]
 intersphinx_mapping = {
     "python": ("https://docs.python.org/3/", None),
@@ -48,12 +30,9 @@ issues_github_path = "wtforms/wtforms"
 html_theme = "werkzeug"
 html_context = {
     "project_links": [
-        ProjectLink(
-            "Discord",
-            "https://discordapp.com/channels/531221516914917387/590290790241009673",
-        ),
-        ProjectLink("PyPI releases", "https://pypi.org/project/WTForms/"),
+        ProjectLink("PyPI Releases", "https://pypi.org/project/WTForms/"),
         ProjectLink("Source Code", "https://github.com/wtforms/wtforms/"),
+        ProjectLink("Discord Chat", "https://discord.gg/F65P7Z9",),
         ProjectLink("Issue Tracker", "https://github.com/wtforms/wtforms/issues/"),
     ]
 }
@@ -70,5 +49,5 @@ html_show_sourcelink = False
 # LaTeX ----------------------------------------------------------------
 
 latex_documents = [
-    ('index', 'WTForms.tex', 'WTForms Documentation', 'WTForms team', 'manual'),
+    ('index', "WTForms-{}.tex".format(version), html_title, author, 'manual'),
 ]
