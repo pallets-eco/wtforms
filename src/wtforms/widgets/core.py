8c01bcf4164d015cd39a3bd444a16aa2c06e0dcc
Serge Hauri <sergehauri@gmail.com>
2020-09-17 18:49:05 +0200

    Added min and max html tag attributes to RangeInput widget.

--- src/wtforms/widgets/core.py
+++ src/wtforms/widgets/core.py
@@ -491,12 +491,18 @@ class RangeInput(Input):
     input_type = "range"
     validation_attrs = ["required", "max", "min", "step"]
 
-    def __init__(self, step=None):
+    def __init__(self, step=None, min=None, max=None):
         self.step = step
+        self.min = min
+        self.max = max
 
     def __call__(self, field, **kwargs):
         if self.step is not None:
             kwargs.setdefault("step", self.step)
+        if self.min is not None:
+            kwargs.setdefault("min", self.min)
+        if self.max is not None:
+            kwargs.setdefault("max", self.max)
         return super().__call__(field, **kwargs)
 
 
